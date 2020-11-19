f71f5b37542d3a2d4672b7a1fe5ed537237baf09
Nikito <nksenofontov97@gmail.com>
2020-11-20 01:02:59 +0200

    two tasks

--- src/main/java/MeasureTheTemperature.java
+++ src/main/java/MeasureTheTemperature.java
@@ -8,6 +8,17 @@ public class MeasureTheTemperature {
         celsius = sc.nextDouble();
         fahrenheit = calkFahrenheit(celsius);
         System.out.println("Ваша температура в Фаренгейтах состалве: " + fahrenheit);
+
+        //Dop task
+        System.out.println("Введите 4-х значное число");
+        int number = sc.nextInt();
+        int res = 0;
+        while (number != 0) {
+            res += number % 10;
+            number /= 10;
+        }
+        System.out.println(res);
+
     }
 
     public static Double calkFahrenheit(double celsius){
